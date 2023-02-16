ac475050b1cc3792c7d361622715a9b189276dc9
amcginn92 <a.mcginn92@gmail.com>
2023-02-15 19:53:46 -0500

    Updated saveButton onClickListener to report errors when fields are empty. Applied greeting only when all fields filled. Still need error checking on program select.

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.assignment2
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -19,8 +21,29 @@ class MainActivity : AppCompatActivity() {
         val saveButton = findViewById<Button>(R.id.button)
 
         saveButton.setOnClickListener{
+//            Toast.makeText(this,"PW: ${textPw.text.toString()}, PWConf: ${textPwConfirm.text.toString()}",Toast.LENGTH_SHORT).show()
+
+            if(textName.text.isBlank()){
+//                Toast.makeText(this,"You done goofed",Toast.LENGTH_SHORT).show()  //maybe use this for select program
+                textName.setError("Please enter a name")
+            }
+            if(textEmail.text.isBlank()){
+                textEmail.setError("Please enter an email")
+            }
+            if(textPw.text.isBlank()){
+                textPw.setError("Please enter a valid password")
+            }
+            if(!(textPwConfirm.text.toString().equals(textPw.text.toString())) ){
+                textPwConfirm.setError("Make sure passwords match")
+            }
+
+
+            if( !(textName.text.isBlank()) && !(textEmail.text.isBlank()) && !(textPw.text.isBlank())
+                && (textPwConfirm.text.toString().equals(textPw.text.toString())) ){
             textView.text = "Welcome, ${textName.text.toString()} to the SignUpForm App"
+            }
         }
         spinner.adapter = ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, arr)
     }
 }
+fun setError(error: CharSequence): Unit {}
